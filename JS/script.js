3bda2a995604bab674a08e5a32642723b489e31d
edgard <edgard.habanbou@hotmail.com>
2023-11-08 00:41:02 +0200

    Added Filters completed, active and All tasks for the tasks table

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 const addTask = document.getElementById("addTask");
+
 const addTaskName = document.getElementById("addTaskName");
 const taskList = document.getElementById("taskList");
 
+// Add task function will add element to table with edit delete and mark as complete buttons
 addTask.addEventListener("click", function () {
   if (addTaskName.value != "") {
     const task = document.createElement("tr");
@@ -13,7 +15,7 @@ addTask.addEventListener("click", function () {
     <td id = "complete"><i class="fa-solid fa-x" style="color: #ff0000;"></i></td>
     <td> <button id="editTask" class="edit-task">Edit</button>
         <button id="deleteTask" class="delete-task">Delete</button>
-        <button id="MarkAsComplete" class="mark-as-complete">Mark As Complete</button>
+        <button id="MarkAsComplete" class="mark-as-complete">Complete</button>
         </td>
     `;
 
@@ -43,5 +45,38 @@ addTask.addEventListener("click", function () {
     addTaskName.value = "";
   } else {
     alert("Please enter a task name");
+  }
+});
+
+// Filter section
+const allTasks = document.getElementById("allTasks");
+const CompletedTasks = document.getElementById("CompletedTasks");
+const ActiveTasks = document.getElementById("ActiveTasks");
+
+const tableRows = document.getElementsByTagName("tr");
+// show all tasks
+allTasks.addEventListener("click", () => {
+  for (let i = 0; i < tableRows.length; i++) {
+    tableRows[i].removeAttribute("hidden");
+  }
+});
+
+CompletedTasks.addEventListener("click", () => {
+  for (let i = 1; i < tableRows.length; i++) {
+    if (!tableRows[i].innerHTML.includes("fa-check")) {
+      tableRows[i].setAttribute("hidden", "true");
+    } else {
+      tableRows[i].removeAttribute("hidden");
+    }
+  }
+});
+
+ActiveTasks.addEventListener("click", () => {
+  for (let i = 1; i < tableRows.length; i++) {
+    if (tableRows[i].innerHTML.includes("fa-check")) {
+      tableRows[i].setAttribute("hidden", "false");
+    } else {
+      tableRows[i].removeAttribute("hidden");
+    }
   }
 });
